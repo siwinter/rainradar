136c3430b52c7c4aea3f4019f9b15cb0073ed233
siwinter <you@example.com>
2021-04-03 20:13:45 +0200

    new version

---
+++ rainradar/__main__.py
@@ -0,0 +1,3 @@
+
+from .rainradar import main
+main()
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name='rainradar',  
+    version='0.0.1',
+    author="siwinter",
+#    author_email="andrew@ao.gl",
+    description="A virtual rainsensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/siwinter/rainradar",
+    packages=["rainradar"],
+    entry_points = {
+        "console_scripts": ['rainradar = rainradar.rainradar:main']
+    },
+    install_requires=[
+		'tornado', 
+		'paho-mqtt'
+		],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
